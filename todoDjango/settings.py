e5deadcb2bf94f14dff1a3431575473d8904b891
smairyu1203 <smairyu_1203@yahoo.co.jp>
2020-08-02 22:15:02 +0900

    cors編集

--- todoDjango/settings.py
+++ todoDjango/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'todo.apps.TodoConfig',
-    'django-cors-headers',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
