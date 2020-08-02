b91e9c07c4895697ad1432c4ef1919dfb5bf4ec4
smairyu1203 <smairyu_1203@yahoo.co.jp>
2020-08-02 17:41:49 +0900

    db設定

--- todoDjango/settings.py
+++ todoDjango/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'todoDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todo_django',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
